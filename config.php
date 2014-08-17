6f5f1513f3f62b85f6576948343bcfaa6d6addb8
intothevoid <karank@outlook.com>
2014-08-18 00:05:01 +0930

    Set up basic connection params

--- config.php
+++ config.php
@@ -1,10 +1,12 @@
 <?php
 
-$hostname = "HOSTNAME";
-$username = "USERNAME";
-$password = "PASSWORD";
-$database = "DATABASE";
+$hostname = "localhost";
+$username = "admin";
+$password = "passpass";
+$database = "feedfollow";
 
+// Connect to database
 $link = mysql_connect($hostname, $username, $password) or die("Cannot connect to the database!");
 mysql_select_db($database) or die("Cannot select database");
+
 ?>
